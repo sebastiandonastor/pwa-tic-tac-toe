e04f149c51a418c2c2db3719a07ee41001aec1a0
sebastiandona <sebastiandonastor@gmail.com>
2024-02-15 04:35:01 -0400

    wrapping up details

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,13 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {  NbThemeModule } from '@nebular/theme';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(),
-    importProvidersFrom(NbThemeModule.forRoot({ name: 'cosmic' })), provideServiceWorker('ngsw-worker.js', {
+  provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     }), provideServiceWorker('ngsw-worker.js', {
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -37,14 +37,14 @@ export class BoardComponent implements OnInit {
     return;
 
     let lines = [
-      [1,2,3],
-      [4,5,6],
-      [7,8,9],
-      [1,5,9],
-      [3,5,7],
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
       [1,4,7],
-      [3,6,9],
-      [2,5,8]
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
     ];
  
     let previousPlayer = !this.isPlayerOneTurn ? 'X' : 'O';
