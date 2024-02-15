17c920c33fd8c42d0faa181fd75beee420c03e44
sebastiandona <sebastiandonastor@gmail.com>
2024-02-15 03:42:07 -0400

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,5 +11,8 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(NbThemeModule.forRoot({ name: 'cosmic' })), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })]
 };
